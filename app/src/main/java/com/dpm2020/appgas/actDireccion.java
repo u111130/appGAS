6a2d22f87944d51f854a4f55d636f5addf982431
ee-escobar <60504286+ee-escobar@users.noreply.github.com>
2020-08-16 19:28:17 -0500

    direccion y login

---
+++ app/src/main/java/com/dpm2020/appgas/actDireccion.java
@@ -0,0 +1,14 @@
+package com.dpm2020.appgas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actDireccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_direccion);
+    }
+}
