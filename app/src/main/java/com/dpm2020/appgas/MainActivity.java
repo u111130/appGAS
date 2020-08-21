9a41e6e8fdbc6d3d30bcd5d3736009281b720b2b
Manuel Temple <manueltemple@gmail.com>
2020-08-20 23:15:56 -0500

    add splash

--- app/src/main/java/com/dpm2020/appgas/MainActivity.java
+++ app/src/main/java/com/dpm2020/appgas/MainActivity.java
@@ -7,13 +7,10 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
+        /* TODO: sincronizacion inicial */
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_main);
-
-
     }
 }
