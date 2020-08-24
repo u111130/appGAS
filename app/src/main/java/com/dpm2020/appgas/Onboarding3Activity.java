31a60af4ef0a5618f809750fc1fe812de8ef6419
Manuel Temple <manueltemple@gmail.com>
2020-08-24 18:00:59 -0500

    onboarding 3

---
+++ app/src/main/java/com/dpm2020/appgas/Onboarding3Activity.java
@@ -0,0 +1,27 @@
+package com.dpm2020.appgas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.dpm2020.appgas.ui.login.LoginActivity;
+
+public class Onboarding3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding3);
+
+        final TextView nextButton = findViewById(R.id.btn_next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+    }
+}
