20b26e4d7a306f9cd3084334bc9d1332e09a1bed
Manuel Temple <manueltemple@gmail.com>
2020-08-24 17:59:59 -0500

    onboarding 2

---
+++ app/src/main/java/com/dpm2020/appgas/Onboarding2Activity.java
@@ -0,0 +1,28 @@
+package com.dpm2020.appgas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Onboarding2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding2);
+
+        final TextView nextButton = findViewById(R.id.btn_next);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Onboarding3Activity.class));
+            }
+        });
+    }
+}
