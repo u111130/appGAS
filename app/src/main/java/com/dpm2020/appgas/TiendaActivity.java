3aebcc97c478a62f738955f4a16f7130bf786830
Manuel Temple <manueltemple@gmail.com>
2020-09-04 12:44:22 -0500

    add dependency volley

--- app/src/main/java/com/dpm2020/appgas/TiendaActivity.java
+++ app/src/main/java/com/dpm2020/appgas/TiendaActivity.java
@@ -1,8 +1,13 @@
 package com.dpm2020.appgas;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
@@ -10,10 +15,30 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.dpm2020.appgas.data.TuGasPreference;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class actTienda extends AppCompatActivity {
+    private final String URL_LIST_PRODUCTS = "https://apigas.komanda.pe/orders/product";
+    private TuGasPreference mTuGasPreference;
+
+    public actTienda(Context context) {
+        this.mTuGasPreference =new TuGasPreference(context);
+    }
 
     Spinner spDirecciones;
     String direcciones[] = {"Jr San Martin 345 Miraflores", "Calle 4 156, San Isidro"};
@@ -28,6 +53,49 @@ public class actTienda extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_tienda);
         asignarReferencias();
+    }
+
+    private void wsGetProducts () {
+        JSONObject jsonBody = new JSONObject();
+        EditText usernameEditText = findViewById(R.id.txtUsername);
+        EditText passwordEditText = findViewById(R.id.password);
+
+
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL_LIST_PRODUCTS, null, new Response.Listener<JSONArray>() {
+            public Map<String, String> getHeaders() {
+                Map<String, String> header = new HashMap<String, String>();
+                String token = prefs.getString("token", null);
+                Log.i("TUGAS", token);
+                header.put("Authorization", "JWT "+ token);
+                return header;
+            }
+
+            @Override
+            public void onResponse(JSONArray response) {
+                //loadingHide();
+                // JSONObject jsonObject = JSONObject(response);
+                Log.i("TUGAS", response.toString());
+                /*
+                SharedPreferences.Editor editor = prefs.edit();
+
+                editor.putString("token", response.getString("token"));
+                editor.putString("user", response.getString("user"));
+                editor.commit();
+
+                 */
+                startActivity(new Intent(getApplicationContext(), actTienda.class));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //loadingHide();
+                Toast.makeText(getApplicationContext(), "Lo sentimos, no se encontro productos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 
     private void asignarReferencias() {
