78aff376a492577d91387b1a78b9c33e1fa6071b
Manuel Temple <manueltemple@gmail.com>
2020-09-07 00:16:02 -0500

    implement webservice login + tienda

--- app/src/main/java/com/dpm2020/appgas/TiendaActivity.java
+++ app/src/main/java/com/dpm2020/appgas/TiendaActivity.java
@@ -1,37 +1,26 @@
 package com.dpm2020.appgas;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.dpm2020.appgas.data.TuGasPreference;
+import com.dpm2020.appgas.network.service.TiendaService;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
-public class TiendaActivity extends AppCompatActivity {
-    private final String URL_LIST_PRODUCTS = "https://apigas.komanda.pe/orders/product";
-    private TuGasPreference mTuGasPreference;
+public class TiendaActivity extends BaseActivity {
+    TiendaActivity activity;
+    TiendaService services;
 
     Spinner spDirecciones;
     String direcciones[] = {"Jr San Martin 345 Miraflores", "Calle 4 156, San Isidro"};
@@ -43,77 +32,42 @@ public class TiendaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.mTuGasPreference = new TuGasPreference(getApplicationContext());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_act_tienda);
-        asignarReferencias();
-        wsGetProducts();
+
+        final TiendaActivity activity = this;
+        services = new TiendaService(activity);
+
+        setContentView(R.layout.activity_tienda);
+
+
+        final TextView title = findViewById(R.id.textUsuarioTitulo);
+        title.setText("Hola "+ mTuGasPreference.getUserName());
+        services.getProducts();
     }
 
-    private void wsGetProducts () {
-        JSONObject jsonBody = new JSONObject();
-        EditText usernameEditText = findViewById(R.id.txtUsername);
-        EditText passwordEditText = findViewById(R.id.password);
-
-        final String token = mTuGasPreference.getToken();
-
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL_LIST_PRODUCTS, null, new Response.Listener<JSONArray>() {
-            public Map<String, String> getHeaders() {
-                Map<String, String> header = new HashMap<String, String>();
-                Log.i("TUGAS", token);
-                header.put("Authorization", "JWT "+ token);
-                return header;
-            }
-
-            @Override
-            public void onResponse(JSONArray response) {
-                //loadingHide();
-                // JSONObject jsonObject = JSONObject(response);
-                Log.i("TUGAS", response.toString());
-                /*
-                SharedPreferences.Editor editor = prefs.edit();
-
-                editor.putString("token", response.getString("token"));
-                editor.putString("user", response.getString("user"));
-                editor.commit();
-
-                 */
-                startActivity(new Intent(getApplicationContext(), TiendaActivity.class));
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //loadingHide();
-                Toast.makeText(getApplicationContext(), "Lo sentimos, no se encontro productos", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(request);
-    }
-
-    private void asignarReferencias() {
+    public void asignarReferencias(JSONArray products) {
+        // TODO: GUARDAR PRODUCTOS EN BASE DE DATOS LOCAL
 
         spDirecciones = findViewById(R.id.spDireccionPed);
         ArrayAdapter adapter = new ArrayAdapter (this, android.R.layout.simple_list_item_activated_1, direcciones);
         spDirecciones.setAdapter(adapter);
 
-        ArrayList<String[]> lista = new ArrayList<>();
-        String[] prod1 = {"Envasado 5kg","S/. 20.00","Dirigido a los que gustan del camping y a los que requieren emplear el gas en circunstancias donde, que por su tamaño y capacidad, resulta una gran ventaja.","1"};
-        String[] prod2 = {"Envasado 10kg","S/. 30.00","De uso doméstico. Tenemos presentaciones para todo tipo de reguladores.","2"};
-        String[] prod3 = {"Envasado 15kg","S/. 40.00","Destinado para una compra reflexiva, pues insume un menor precio por kilo.","3"};
 
-        lista.add(prod1);
-        lista.add(prod2);
-        lista.add(prod3);
 
         ArrayList<HashMap<String,String>> productos = new ArrayList<>();
-        for (String[] producto : lista) {
+        for (int i=0; i<products.length(); i++)
+        {
             HashMap<String,String> datosProducto = new HashMap<>();
-            datosProducto.put("Producto",producto[0]);
-            datosProducto.put("Precio",producto[1]);
-            datosProducto.put("Descripcion",producto[2]);
-            datosProducto.put("Id",producto[3]);
+            try {
+                JSONObject product = products.getJSONObject(i);
+
+                datosProducto.put("Producto", product.getString("name"));
+                datosProducto.put("Precio", product.getString("price"));
+                datosProducto.put("Descripcion", product.getString("description"));
+                datosProducto.put("Id", product.getString("id"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
             productos.add(datosProducto);
         }
--- app/src/main/java/com/dpm2020/appgas/ui/login/LoginActivity.java
+++ app/src/main/java/com/dpm2020/appgas/ui/login/LoginActivity.java
@@ -1,47 +1,35 @@
 package com.dpm2020.appgas.ui.login;
 
-import androidx.lifecycle.ViewModelProviders;
-
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+import com.dpm2020.appgas.BaseActivity;
 import com.dpm2020.appgas.R;
-import com.dpm2020.appgas.TiendaActivity;
-import com.dpm2020.appgas.data.TuGasPreference;
+import com.dpm2020.appgas.network.service.LoginService;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class LoginActivity extends AppCompatActivity {
-
-    private LoginViewModel loginViewModel;
-    private final String URL_LOGIN = "https://apigas.komanda.pe/auth/token/auth";
-    private TuGasPreference mTuGasPreference;
+public class LoginActivity extends BaseActivity {
+    LoginActivity activity;
+    LoginService services;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        this.mTuGasPreference = new TuGasPreference(getApplicationContext());
         super.onCreate(savedInstanceState);
+
+        final LoginActivity activity = this;
+        services = new LoginService(activity);
+
+        Boolean noShowOnboarding = mTuGasPreference.getOnBoarding();
+        Log.i("TUGAS", noShowOnboarding ? "NO SHOW" : "SHOW");
+        mTuGasPreference.setOnBoarding(true);
+        noShowOnboarding = mTuGasPreference.getOnBoarding();
+        Log.i("TUGAS", noShowOnboarding ? "NO SHOW" : "SHOW");
+
         setContentView(R.layout.activity_login);
-        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
 
         final EditText usernameEditText = findViewById(R.id.txtUsername);
         final EditText passwordEditText = findViewById(R.id.password);
@@ -50,6 +38,9 @@ public class LoginActivity extends AppCompatActivity {
 
         final TextView btn_register = findViewById(R.id.textView13);
         final TextView btn_register2 = findViewById(R.id.textView14);
+
+        usernameEditText.setText("manueltemple@gmail.com");
+        passwordEditText.setText("123123");
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,148 +58,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        /*
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                //loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
-
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
-        */
-
-        /*
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-        */
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String username = String.valueOf(usernameEditText.getText());
+                String password = String.valueOf(passwordEditText.getText());
 
-
-                Toast.makeText(getApplicationContext(), "clic", Toast.LENGTH_SHORT).show();
-                /*
-                startActivity(new Intent(getApplicationContext(), TiendaActivity.class));
-                */
-
-                loadingProgressBar.setVisibility(View.VISIBLE);
-
-                wsLogin();
-                /*
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-                 */
+                services.login(username, password);
             }
         });
-    }
-    /*
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-    }
-
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
-    }
-    */
-
-    private void loadingHide() {
-        ProgressBar loadingProgressBar = findViewById(R.id.loading);
-        loadingProgressBar.setVisibility(View.INVISIBLE);
-    }
-
-    private void wsLogin() {
-        JSONObject jsonBody = new JSONObject();
-        EditText usernameEditText = findViewById(R.id.txtUsername);
-        EditText passwordEditText = findViewById(R.id.password);
-
-        try {
-            jsonBody.put("username", usernameEditText.getText());
-            jsonBody.put("password", passwordEditText.getText());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        final String requestBody = jsonBody.toString();
-
-        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, URL_LOGIN, jsonBody, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                loadingHide();
-                // JSONObject jsonObject = JSONObject(response);
-                Log.i("TUGAS", response.toString());
-                try {
-                    mTuGasPreference.setToken(response.getString("token"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                startActivity(new Intent(getApplicationContext(), TiendaActivity.class));
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                loadingHide();
-                Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
     }
 }
