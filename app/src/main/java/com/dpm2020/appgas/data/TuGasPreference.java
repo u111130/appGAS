fbef6abd54250b845578c6488540f3295c5411a2
Manuel Temple <manueltemple@gmail.com>
2020-09-07 00:13:20 -0500

    add share preferences

--- app/src/main/java/com/dpm2020/appgas/data/TuGasPreference.java
+++ app/src/main/java/com/dpm2020/appgas/data/TuGasPreference.java
@@ -8,60 +8,88 @@ import com.dpm2020.appgas.R;
 import static android.content.Context.MODE_PRIVATE;
 
 public class TuGasPreference  {
-    private final Context goContext;
+    private final Context mContext;
+    private static final String SHARED_ONBOARDING = "ONBOARDING";
     private static final String SHARED_TOKEN = "TOKEN";
     private static final String SHARED_CURRENT_USER = "USER";
+    private static final String SHARED_CURRENT_USER_NAME = "USER_NAME";
+    private static final String SHARED_CURRENT_USER_ID = "USER_NAME_ID";
 
-    public TuGasPreference(Context poContext) {
-        this.goContext = poContext;
+
+    public TuGasPreference(Context context) {
+        this.mContext = context;
     }
 
+    public SharedPreferences getSharedPreferences() {
+        return this.mContext.getSharedPreferences(mContext.getString(R.string.app_name), MODE_PRIVATE);
+    }
 
-    public Context getContext() {
-        return this.goContext;
+    public SharedPreferences.Editor getEditoSharedPreference() {
+        return getSharedPreferences().edit();
     }
 
     public String getToken() {
         return getString(SHARED_TOKEN);
     }
 
-    public void setToken(String token) {
-        putString(SHARED_TOKEN, token);
+    public void setToken(String value) {
+        putString(SHARED_TOKEN, value);
     }
 
-    /*
-    public void setUser(User user) {
-        putString(SHARED_CURRENT_USER, JsonUtil.getStringToJson(user));
+    public String getUserId() {
+        return getString(SHARED_CURRENT_USER_ID);
     }
 
-    public User getUser() {
-        String json = getString(SHARED_CURRENT_USER);
-        if (json.isEmpty()) return null;
-        return JsonUtil.getJsonToString(json, User.class);
+    public void setUserId(String value) {
+        putString(SHARED_CURRENT_USER_ID, value);
     }
-    */
 
-    public void putString(String psKey, String psValue) {
-        SharedPreferences sharedPref = getContext().getSharedPreferences(goContext.getString(R.string.app_name), MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(psKey, psValue);
+    public String getUserName() {
+        return getString(SHARED_CURRENT_USER_NAME);
+    }
+
+    public void setUserName(String value) {
+        putString(SHARED_CURRENT_USER_NAME, value);
+    }
+
+    public Boolean getOnBoarding() {
+        return getBoolean(SHARED_ONBOARDING);
+    }
+
+    public void setOnBoarding(Boolean value) {
+        putBoolean(SHARED_ONBOARDING, value);
+    }
+
+    public String getString(String key) {
+        SharedPreferences sharedPref = getSharedPreferences();
+        return sharedPref.getString(key, "");
+    }
+
+    public void putString(String key, String value) {
+        SharedPreferences.Editor editor = getEditoSharedPreference();
+        editor.putString(key, value);
         editor.apply();
     }
 
-    public String getString(String psKey) {
-        SharedPreferences sharedPref = getContext().getSharedPreferences(goContext.getString(R.string.app_name), MODE_PRIVATE);
-        return sharedPref.getString(psKey, "");
+    public boolean getBoolean(String key) {
+        SharedPreferences sharedPref = getSharedPreferences();
+        return sharedPref.getBoolean(key, false);
     }
 
-    public void putLong(String psKey, long psValue) {
-        SharedPreferences sharedPref = getContext().getSharedPreferences(goContext.getString(R.string.app_name), MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putLong(psKey, psValue);
+    public void putBoolean(String key, Boolean value) {
+        SharedPreferences.Editor editor = getEditoSharedPreference();
+        editor.putBoolean(key, value);
         editor.apply();
     }
 
     public long getLong(String psKey) {
-        SharedPreferences sharedPref = getContext().getSharedPreferences(goContext.getString(R.string.app_name), MODE_PRIVATE);
+        SharedPreferences sharedPref = getSharedPreferences();
         return sharedPref.getLong(psKey, -1);
+    }
+
+    public void putLong(String psKey, long psValue) {
+        SharedPreferences.Editor editor = getEditoSharedPreference();
+        editor.putLong(psKey, psValue);
+        editor.apply();
     }
 }
